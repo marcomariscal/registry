2a7af6445719710e6841c07c0b44b28faa638281
marcomariscal <marco.a.mariscal@gmail.com>
2022-01-06 17:03:53 -0500

    update to correct contract name

--- tasks/deploy/registry.ts
+++ tasks/deploy/registry.ts
@@ -5,7 +5,7 @@ import { Registry__factory } from "../../src/types/factories/Registry__factory";
 import { Registry } from "../../src/types/Registry";
 
 task("deploy:Registry").setAction(async (taskArgs: TaskArguments, { ethers }) => {
-  const registryFactory = <Registry__factory>await ethers.getContractFactory("Greeter");
+  const registryFactory = <Registry__factory>await ethers.getContractFactory("Registry");
   const registry = <Registry>await registryFactory.deploy();
   await registry.deployed();
   console.log("Registry deployed to: ", registry.address);
