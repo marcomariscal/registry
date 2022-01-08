2973708045f9f7edc75665cac0b418f0cea3cc95
marcomariscal <marco.a.mariscal@gmail.com>
2022-01-08 09:20:27 -0500

    update supply

--- contracts/ERC20Mock.sol
+++ contracts/ERC20Mock.sol
@@ -4,6 +4,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 10 * (10 ^ 18));
+        _mint(msg.sender, 10 * (10**18));
     }
 }
